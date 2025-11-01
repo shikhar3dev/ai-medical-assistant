36f395111d01b1b178078558ec145bdeb5a6a385
SHIKHAR SINGH <shikhar71615@gmail.com>
2025-11-01 08:16:46 +0530

    AI Medical Assistant - Privacy-Preserving Federated Learning for Healthcare AI

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,4 +1,4 @@
-"""Data Preprocessing Module"""
+"""Data Preprocessing Module."""
 
 from .data_loader import load_heart_disease, load_diabetes, download_datasets
 from .preprocessor import DataPreprocessor
